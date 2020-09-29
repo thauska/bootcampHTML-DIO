7482a65b4aececaa3c6c570f34875a862694ae20
Thayana Mamoré <thayanacmamore@gmail.com>
2020-09-29 15:48:25 -0300

    Criar comida

--- cloneJogoCobra/script.js
+++ cloneJogoCobra/script.js
@@ -2,13 +2,17 @@ let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
 let box = 32
 let snake = []
-
-let direction = "right"
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box, 
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 function criarBG() {
     context.fillStyle = "lightgreen"
@@ -20,6 +24,11 @@ function criarCobrinha() {
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function criarComida() {
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +49,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    criarComida();
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
