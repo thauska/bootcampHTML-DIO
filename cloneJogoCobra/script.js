6d76a64172535100aac050f259717fe47ae6e24b
Thayana Mamoré <thayanacmamore@gmail.com>
2020-09-29 15:25:15 -0300

    Definir movimentos da cobrinha

--- cloneJogoCobra/script.js
+++ cloneJogoCobra/script.js
@@ -3,6 +3,8 @@ let context = canvas.getContext("2d")
 let box = 32
 let snake = []
 
+let direction = "right"
+
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -20,5 +22,27 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y:snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
+
