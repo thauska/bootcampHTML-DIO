b1d3a4b69973416972c52fb06a25e5d15eaf33ce
cassio8186 <cassio8186@gmail.com>
2020-10-26 20:06:12 -0300

    fix: Correção de erro
    [Vídeo demonstrando erro](https://youtu.be/yud00d7XyR0)
    
    Jogando o joguinho da cobrinha, percebi que ao realizar um movimento
    eu morria sem justificativa.
    
    Percebi então que em a cada rodada era verificado a direção da cobra.
    
    Logo me dei conta de que era possível mudar de direção mais de uma vez
    a cada rodada.
    O que fazia com que caso a cobrinha estivesse indo pra esquerda,
    mudasse para baixo e para direita, a cobrinha morreria pois
    entraria em colisão.
    
    Corrigi o erro adicionando uma verificação se já foi realizado
    uma mudança de direção em uma mesma rodada.
    e durante cada rodada essa variável é modificada para false

--- cloneJogoCobra/script.js
+++ cloneJogoCobra/script.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+// Verificação se a direção foi trocada na rodada
+let direcaoTrocadaNaRodada = false;
 let snake = [];
 snake[0] = {
   x: 8 * box,
@@ -33,14 +35,35 @@ function criarComida() {
 document.addEventListener("keydown", update);
 
 function update(event) {
-  if (event.keyCode == 37 && direction != "right") direction = "left";
-  if (event.keyCode == 38 && direction != "down") direction = "up";
-  if (event.keyCode == 39 && direction != "left") direction = "right";
-  if (event.keyCode == 40 && direction != "up") direction = "down";
+  //Trocará direção apenas se a direção ainda não foi trocada na rodada
+  if (!direcaoTrocadaNaRodada) {
+    if (event.keyCode == 37 && direction != "right") {
+      direction = "left";
+      direcaoTrocadaNaRodada = true;
+      console.log("Direção modificada para " + direction);
+    }
+    if (event.keyCode == 38 && direction != "down") {
+      direction = "up";
+      direcaoTrocadaNaRodada = true;
+      console.log("Direção modificada para " + direction);
+    }
+    if (event.keyCode == 39 && direction != "left") {
+      direction = "right";
+      direcaoTrocadaNaRodada = true;
+      console.log("Direção modificada para " + direction);
+    }
+    direcaoTrocadaNaRodada = true;
+    if (event.keyCode == 40 && direction != "up") {
+      direction = "down";
+      direcaoTrocadaNaRodada = true;
+      console.log("Direção modificada para " + direction);
+    }
+  }
 }
 
 function iniciarJogo() {
   //plano cartesiano
+  console.log("Direção atual:" + direction);
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -78,6 +101,7 @@ function iniciarJogo() {
   };
 
   snake.unshift(newHead);
+  direcaoTrocadaNaRodada = false;
 }
 
 let jogo = setInterval(iniciarJogo, 100);
